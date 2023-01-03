dbbeff0577daa4dbdff3a1682354cf0183683a7a
mikhail.madoyan <mikhail.madoyan@gmail.com>
2023-01-03 23:32:09 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    abstract class Weapon
+    {
+        public abstract int Damage { get; }
+
+        public abstract void Fire();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{GetType().Name} Damage: {Damage}");
+        }
+    }
+
+    class Gun : Weapon
+    {
+        public override int Damage { get { return 5; } }
+
+        public override void Fire()
+        {
+            Console.WriteLine("Pif!");
+        }
+    }
+
+    class LaserGun : Weapon
+    {
+        public override int Damage { get { return 8; } }
+
+        public override void Fire()
+        {
+            Console.WriteLine("Piu!");
+        }
+    }
+
+    class Bow : Weapon
+    {
+        public override int Damage => 3;
+
+        public override void Fire()
+        {
+            Console.WriteLine("Chpunk!");
+        }
+    }
+
+    class Player
+    {
+        public void Fire(Weapon weapon)
+        {
+            weapon.Fire();
+        }
+
+        public void CheckInfo(Weapon weapon)
+        {
+            weapon.ShowInfo();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            Weapon[] inventory = { new Gun(), new LaserGun(), new Bow() };
+
+            foreach (var item in inventory)
+            {
+                player.CheckInfo(item);
+                player.Fire(item);
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
